30d822311ae74e44d64449e348b594b55cce1f03
vothanhdanh <999conan999@gmail.com>
2022-04-18 00:41:37 +0700

    v0.1

--- wp-content/plugins/zsharevn-landing-page/index.php
+++ wp-content/plugins/zsharevn-landing-page/index.php
@@ -8,5 +8,51 @@ Author: zshare
 Author URI: zshare.vn
 License: GPL
 */
+add_action( 'init', 'create_post_type_zshare' );
+function create_post_type_zshare(){
+    $label = array(
+        'name' => 'Landing Page', //Tên post type dạng số nhiều
+        'singular_name' => 'Landing Page' //Tên post type dạng số ít
+    );
+    $args = array(
+        'labels' => $label, //Gọi các label trong biến $label ở trên
+        // 'description' => 'cccc', //Mô tả của post type
+        'supports' => array(
+        'title',
+        'editor',
+        ),
+        'rewrite' => array('slug' => 'vi'),
+        'taxonomies' => array(), //Các taxonomy được phép sử dụng để phân loại nội dung
+        'hierarchical' => true, //Cho phép phân cấp, nếu là false thì post type này giống như Post, true thì giống như Page
+        'public' => true, //Kích hoạt post type
+        'show_ui' => false, //Hiển thị khung quản trị như Post/Page
+        'can_export' => true, //Có thể export nội dung bằng Tools -> Export
+        'exclude_from_search' => true, //Loại bỏ khỏi kết quả tìm kiếm
+        'publicly_queryable' => true, //Hiển thị các tham số trong query, phải đặt true
+        // 'capability_type' => 'page' //
+    );
+    register_post_type('zshare', $args);
+}
+add_filter( 'template_include', 'include_template_function', 1 );
+function include_template_function( $template_path ) {
+    if ( get_post_type() == 'zshare' ) {
+        if ( is_single() ) {
+            if ( $theme_file = locate_template( array ( 'single-zshare.php' ) ) ) {
+                $template_path = $theme_file;
+            } else {
+                $template_path = plugin_dir_path( __FILE__ ) . '/single-zshare.php';
+            }
+        }
+    }
+    return $template_path;
+}
+
+add_action('admin_menu', 'test_plugin_setup_menu');
+function test_plugin_setup_menu(){
+    add_menu_page( 'Tạo landing page', 'Langding page', 'manage_options', 'create-landing-page-by-zshare_vn', 'test_init',plugin_dir_url(__FILE__).'/icon.png',3 );
+}
  
+function test_init(){
+    echo "<h1>Hello World!</h1>";
+}
 ?>
--- wp-content/themes/danhdev_1/templates/ajax/admin.php
+++ wp-content/themes/danhdev_1/templates/ajax/admin.php
@@ -905,7 +905,7 @@ function is_table_created($table_name){
 // var_dump(is_table_created('wp_test_danh'));
 
 
-
+// tạo 1 post type mới
 function create_pagexx($titleS,$contentS,$statusS,$metaA,$thumnailS){
 
     $label = array(
@@ -929,15 +929,15 @@ function create_pagexx($titleS,$contentS,$statusS,$metaA,$thumnailS){
         'taxonomies' => array(), //Các taxonomy được phép sử dụng để phân loại nội dung
         'hierarchical' => true, //Cho phép phân cấp, nếu là false thì post type này giống như Post, true thì giống như Page
         'public' => true, //Kích hoạt post type
-        'show_ui' => false, //Hiển thị khung quản trị như Post/Page
-        'show_in_menu' => false, //Hiển thị trên Admin Menu (tay trái)
-        'show_in_nav_menus' => false, //Hiển thị trong Appearance -> Menus
-        'show_in_admin_bar' => false, //Hiển thị trên thanh Admin bar màu đen.
-        'menu_position' => 5, //Thứ tự vị trí hiển thị trong menu (tay trái)
-        'menu_icon' => '', //Đường dẫn tới icon sẽ hiển thị
-        'can_export' => true, //Có thể export nội dung bằng Tools -> Export
-        'has_archive' => true, //Cho phép lưu trữ (month, date, year)
-        'exclude_from_search' => true, //Loại bỏ khỏi kết quả tìm kiếm
+        // 'show_ui' => false, //Hiển thị khung quản trị như Post/Page
+        // 'show_in_menu' => false, //Hiển thị trên Admin Menu (tay trái)
+        // 'show_in_nav_menus' => false, //Hiển thị trong Appearance -> Menus
+        // 'show_in_admin_bar' => false, //Hiển thị trên thanh Admin bar màu đen.
+        // 'menu_position' => 5, //Thứ tự vị trí hiển thị trong menu (tay trái)
+        // 'menu_icon' => '', //Đường dẫn tới icon sẽ hiển thị
+        // 'can_export' => true, //Có thể export nội dung bằng Tools -> Export
+        // 'has_archive' => true, //Cho phép lưu trữ (month, date, year)
+        // 'exclude_from_search' => true, //Loại bỏ khỏi kết quả tìm kiếm
         'publicly_queryable' => true, //Hiển thị các tham số trong query, phải đặt true
         'capability_type' => 'post' //
     );
