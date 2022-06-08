5b865ee742111532e604408e65c2d66fe7d9bfc7
vothanhdanh <999conan999@gmail.com>
2022-06-08 09:13:10 +0700

    xxxxxxxsssssss

--- wp-content/plugins/zsharevn-landing-page/single-zshare.php
+++ wp-content/plugins/zsharevn-landing-page/single-zshare.php
@@ -62,8 +62,8 @@
     <link href="<?php echo $plugin_setup->icon_url_192; ?>" rel="apple-touch-icon">
     <link href="<?php echo $plugin_setup->icon_url_192; ?>" rel="apple-touch-icon-precomposed">
     <link rel="stylesheet" charset="UTF-8" href="<?php echo $home_url; ?>/wp-content/plugins/zsharevn-landing-page/css01.css" />
-    <script defer="defer" src="<?php echo $home_url; ?>/wp-content/plugins/zsharevn-landing-page/static/js/main.b1459461.js"></script>
-    <link href="<?php echo $home_url; ?>/wp-content/plugins/zsharevn-landing-page/static/css/main.ff8fcf64.css" rel="stylesheet">
+    <script defer="defer" src="<?php echo $home_url; ?>/wp-content/plugins/zsharevn-landing-page/static/js/main.a2452b78.js"></script>
+    <link href="<?php echo $home_url; ?>/wp-content/plugins/zsharevn-landing-page/static/css/main.f11a798f.css" rel="stylesheet">
     <!-- all -->
     <?php echo $plugin_setup->code_header; ?>
     <?php echo $metaA->code_header; ?>
